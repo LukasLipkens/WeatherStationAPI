aac99f7eeb57686375ed90c18417223c09ea5b77
LukasLipkens <84568981+LukasLipkens@users.noreply.github.com>
2024-10-09 08:59:52 +0200

    toevoegen van de dataContext en connection string

--- WeatherStationAPI/Program.cs
+++ WeatherStationAPI/Program.cs
@@ -1,12 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using WeatherStationAPI.Data;
+using WeatherStationAPI.Interfaces;
+using WeatherStationAPI.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// add automapper for the Dto's
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Add services to the container.
+builder.Services.AddScoped<IMeasurementRepository, MeasurementRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LaptopConnectionString"));
+});
 
 var app = builder.Build();
 

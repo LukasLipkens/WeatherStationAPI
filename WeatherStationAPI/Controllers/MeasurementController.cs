c4cc1991255c717c006c27777328b83a6d1af363
viktorDries <viktor.dries@student.ucll.be>
2024-10-09 15:24:23 +0200

    Controller + repository

---
+++ WeatherStationAPI/Controllers/MeasurementController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherStationAPI.Interfaces;
+using WeatherStationAPI.Models;
+
+namespace WeatherStationAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class MeasurementController : Controller
+    {
+        private readonly IMeasurementRepository _measurementRepository;
+        public MeasurementController(IMeasurementRepository measurementRepository)
+        {
+            _measurementRepository = measurementRepository;
+        }
+
+        [HttpGet("station/{stationId}/sensor/{sensorId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Measurement>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetMeasurementsFromStationSensor(int stationId, int sensorId)
+        {
+            // Haal metingen op via de repository
+            var measurements = _measurementRepository.GetAllMeasurementsFromStationSensor(stationId, sensorId);
+
+            // Controleer of er resultaten zijn
+            if (measurements == null || !measurements.Any())
+            {
+                return NotFound("Geen metingen gevonden voor het opgegeven station en sensor.");
+            }
+
+            // Retourneer de metingen
+            return Ok(measurements);
+        }
+    }
+}
--- WeatherStationAPI/Repositories/MeasurementRepository.cs
+++ WeatherStationAPI/Repositories/MeasurementRepository.cs
@@ -1,13 +1,19 @@
-﻿using WeatherStationAPI.Interfaces;
+﻿using WeatherStationAPI.Data;
+using WeatherStationAPI.Interfaces;
 using WeatherStationAPI.Models;
 
 namespace WeatherStationAPI.Repositories
 {
     public class MeasurementRepository : IMeasurementRepository
     {
-        public List<Measurement> GetAllMeasurementsFromStationSensor(int stationId, int sensorId)
+        private readonly DataContext _dataContext;
+
+        public MeasurementRepository(DataContext dataContext)
         {
-            throw new NotImplementedException();
+            _dataContext = dataContext;
         }
+
+        public List<Measurement> GetAllMeasurementsFromStationSensor(int stationId, int sensorId)
+            => _dataContext.Measurements.Where(m => m.StationId == stationId && m.SensorId == sensorId).ToList();
     }
 }

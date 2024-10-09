fdbf161bbf3d050886f4df3019f9bf135455fec0
LukasLipkens <84568981+LukasLipkens@users.noreply.github.com>
2024-10-09 10:27:40 +0200

    oepsie

--- WeatherStationAPI/Data/DataContext.cs
+++ WeatherStationAPI/Data/DataContext.cs
@@ -4,18 +4,6 @@ namespace WeatherStationAPI.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-        }
 
-        public DbSet<Models.Measurement> Measurements { get; set; }
-        public DbSet<Models.Sensor> Sensors { get; set; }
-        public DbSet<Models.Station> Stations { get; set; }
-        public DbSet<Models.Station_Sensor> Station_Sensors { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // hier komen de relaties
-        }
     }
 }

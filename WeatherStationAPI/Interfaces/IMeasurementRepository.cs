7aa829b23cc4113e5bd949d0dfe477da58cc939b
LukasLipkens <84568981+LukasLipkens@users.noreply.github.com>
2024-10-09 08:38:08 +0200

    toevoegen van entityframework + aanmaken van measurement repo en interface

---
+++ WeatherStationAPI/Interfaces/IMeasurementRepository.cs
@@ -0,0 +1,9 @@
+﻿using WeatherStationAPI.Models;
+
+namespace WeatherStationAPI.Interfaces
+{
+    public interface IMeasurementRepository
+    {
+        List<Measurement> GetAllMeasurementsFromStationSensor(int stationId, int sensorId);
+    }
+}
---
+++ WeatherStationAPI/Repositories/MeasurementRepository.cs
@@ -0,0 +1,13 @@
+﻿using WeatherStationAPI.Interfaces;
+using WeatherStationAPI.Models;
+
+namespace WeatherStationAPI.Repositories
+{
+    public class MeasurementRepository : IMeasurementRepository
+    {
+        public List<Measurement> GetAllMeasurementsFromStationSensor(int stationId, int sensorId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

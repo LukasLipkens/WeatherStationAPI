917ff20c7cde0e981b818e8c7e8caf200d991946
Philip Mulders <philip.mulders@outlook.com>
2024-10-16 15:47:40 +0200

    commit stage

--- WeatherStationAPI/Interfaces/IMeasurementRepository.cs
+++ WeatherStationAPI/Interfaces/IMeasurementRepository.cs
@@ -1,12 +1,9 @@
-﻿using WeatherStationAPI.Dto;
-using WeatherStationAPI.Models;
+﻿using WeatherStationAPI.Models;
 
 namespace WeatherStationAPI.Interfaces
 {
     public interface IMeasurementRepository
     {
-        List<Measurement> GetAllMeasurementsFromStationSensor(int stationId, int sensorId);
-        List<SensorDto> GetMeasurementsFromSensorInTimeRange(int stationId, DateTime start, DateTime end, List<int>? sensorIds = null);
+        IEnumerable<Measurement> GetAllMeasurementsFromStationSensor(int stationId, int sensorId);
     }
 }
-

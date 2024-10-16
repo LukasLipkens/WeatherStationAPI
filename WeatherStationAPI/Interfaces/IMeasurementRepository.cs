2a349273d9f2cccfacfb70a9277e2dc5dce60f48
Philip Mulders <philip.mulders@outlook.com>
2024-10-16 16:11:21 +0200

    Fix Merging conflict to allow measurements by station

--- WeatherStationAPI/Interfaces/IMeasurementRepository.cs
+++ WeatherStationAPI/Interfaces/IMeasurementRepository.cs
@@ -1,9 +1,11 @@
-﻿using WeatherStationAPI.Models;
+﻿using WeatherStationAPI.Dto;
+using WeatherStationAPI.Models;
 
 namespace WeatherStationAPI.Interfaces
 {
     public interface IMeasurementRepository
     {
-        IEnumerable<Measurement> GetAllMeasurementsFromStationSensor(int stationId, int sensorId);
+        List<Measurement> GetAllMeasurementsFromStationSensor(int stationId, int sensorId);
+        List<SensorDto> GetMeasurementsFromSensorInTimeRange(int stationId, DateTime start, DateTime end, List<int>? sensorIds = null);
     }
 }

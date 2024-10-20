a741dc6ee6a9c88fc33274228d67d23ba9ff5569
LukasLipkens <84568981+LukasLipkens@users.noreply.github.com>
2024-10-20 17:00:28 +0200

    MQTT data ontvangen (niet zo een goede manier)

--- WeatherStationMQTT/Program.cs
+++ WeatherStationMQTT/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System.Text;
+
+namespace WeatherStationMQTT
+{
+    class Publisher
+    {
+        static async Task Main(string[] args)
+        {
+            string broker = "www.lukaslipkens.be";
+            int port = 1883;
+            string clientId = Guid.NewGuid().ToString();
+            string username = "station";
+            string password = "Elo-Ict-2024";
+            string topic = "station/data/#";
+
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId(clientId)
+                .WithTcpServer(broker, port)
+                .WithCredentials(username, password)
+                .WithCleanSession()
+                .Build();
+
+            var connectionResult = mqttClient.ConnectAsync(options).Result;
+
+            if(connectionResult.ResultCode == MqttClientConnectResultCode.Success)
+            {
+                Console.WriteLine("Connected to MQTT broker");
+                await mqttClient.SubscribeAsync(topic);
+
+                mqttClient.ApplicationMessageReceivedAsync += e =>
+                {
+                    Console.WriteLine("Received message on topic: " + e.ApplicationMessage.Topic);
+                    Console.WriteLine($"Received message: {Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment)}");
+                    return Task.CompletedTask;
+                };
+
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect to MQTT broker");
+                return;
+            }
+
+            while (true)
+            {
+
+            }
+
+        }
+    }
+}

9f8b5e84a649a85bf18ca473868972c8ae95944f
JaanRogiers <jaan.rogiers@hotmail.com>
2024-11-08 01:16:58 +0100

    created context factory for creating migrations without startup project

---
+++ src/UCLL.Projects.WeatherStations.Shared/Factories/DataContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using UCLL.Projects.WeatherStations.Shared.Data;
+
+namespace UCLL.Projects.WeatherStations.Shared.Factories;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        DbContextOptionsBuilder<DataContext> optionsBuilder = new();
+        optionsBuilder.UseSqlServer("connstr");
+
+        return new(optionsBuilder.Options);
+    }
+}

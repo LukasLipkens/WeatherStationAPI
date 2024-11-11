7f856e4a42a93d902c54704af2d554a34d4ec509
JaanRogiers <jaan.rogiers@hotmail.com>
2024-11-11 21:03:08 +0100

    update StationControllerTests

--- tests/UCLL.Projects.Weatherstations.WebApi.Tests/Controllers/StationControllerTests.cs
+++ tests/UCLL.Projects.Weatherstations.WebApi.Tests/Controllers/StationControllerTests.cs
@@ -1,49 +1,47 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using UCLL.Projects.WeatherStations.Shared.Data.Models;
 using UCLL.Projects.WeatherStations.WebApi.Controllers;
 using UCLL.Projects.WeatherStations.WebApi.Dto;
+using UCLL.Projects.WeatherStations.WebApi.Interfaces.Repositories;
 
 namespace UCLL.Projects.WeatherStations.WebApi.Tests.Controllers
 {
     public class StationControllerTests
     {
-        private Mock<IStationRepository> _stationRepositoryMock;
-        private Mock<IMapper> _mapperMock;
-        private StationController _stationController;
+        private readonly Mock<IStationRepository> _stationRepositoryMock = new();
+        private readonly Mock<IMapper> _mapperMock = new();
+        private readonly StationController _stationController;
 
         public StationControllerTests()
         {
-            _stationRepositoryMock = new Mock<IStationRepository>();
-            _mapperMock = new Mock<IMapper>();
-            _stationController = new StationController(_stationRepositoryMock.Object, _mapperMock.Object);
+            _stationController = new(_stationRepositoryMock.Object, _mapperMock.Object);
         }
 
         [Fact]
         public void StationController_GetAllStations_ReturnOK_WhenStationsExist()
         {
             // Arrange
-            var stations = new List<Station>
-            {
-                new Station { Id = "1", Name = "Station A", Latitude = 40.7128, Longitude = -74.0060, Description = "Weather station in New York" },
-                new Station { Id = "2", Name = "Station B", Latitude = 34.0522, Longitude = -118.2437, Description = "Weather station in Los Angeles" }
-            };
+            List<Station> stations = [
+                new() { Id = "1", Name = "Station A", Latitude = 40.7128, Longitude = -74.0060, Description = "Weather station in New York" },
+                new() { Id = "2", Name = "Station B", Latitude = 34.0522, Longitude = -118.2437, Description = "Weather station in Los Angeles" }
+            ];
 
-            var stationDtos = new List<SimpleStationDto>
-            {
-                new SimpleStationDto { Id = "1", Name = "Station A", Latitude = 40.7128, Longitude = -74.0060, Description = "Weather station in New York" },
-                new SimpleStationDto { Id = "2", Name = "Station B", Latitude = 34.0522, Longitude = -118.2437, Description = "Weather station in Los Angeles" }
-            };
+            List<SimpleStationDto> stationDtos = [
+                new() { Id = "1", Name = "Station A", Latitude = 40.7128, Longitude = -74.0060, Description = "Weather station in New York" },
+                new() { Id = "2", Name = "Station B", Latitude = 34.0522, Longitude = -118.2437, Description = "Weather station in Los Angeles" }
+            ];
 
             _stationRepositoryMock.Setup(repo => repo.GetAllStations()).Returns(stations);
             _mapperMock.Setup(mapper => mapper.Map<List<SimpleStationDto>>(It.IsAny<List<Station>>())).Returns(stationDtos);
 
             // Act
-            var result = _stationController.GetAllStations();
+            IActionResult result = _stationController.GetAllStations();
 
             // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result); // Controleert of resultaat een OkObjectResult is
-            var returnValue = Assert.IsType<List<SimpleStationDto>>(okResult.Value); // Controleert of de waarde van okValue een lijst is van SimpleStationDtos
+            OkObjectResult okResult = Assert.IsType<OkObjectResult>(result); // Controleert of resultaat een OkObjectResult is
+            List<SimpleStationDto> returnValue = Assert.IsType<List<SimpleStationDto>>(okResult.Value); // Controleert of de waarde van okValue een lijst is van SimpleStationDtos
             Assert.Equal(2, returnValue.Count);
             Assert.Equal("Station A", returnValue[0].Name); // Correcte naam
             Assert.Equal("Station B", returnValue[1].Name); // Correcte naam
@@ -58,10 +56,10 @@ namespace UCLL.Projects.WeatherStations.WebApi.Tests.Controllers
             _mapperMock.Setup(mapper => mapper.Map<List<SimpleStationDto>>(It.IsAny<List<Station>>())).Returns(new List<SimpleStationDto>());
 
             // Act
-            var result = _stationController.GetAllStations();
+            IActionResult result = _stationController.GetAllStations();
 
             // Assert
-            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result); // Controleert of het resultaat een NotFoundObjectResult is
+            NotFoundObjectResult notFoundResult = Assert.IsType<NotFoundObjectResult>(result); // Controleert of het resultaat een NotFoundObjectResult is
             Assert.Equal("Geen metingen gevonden voor het opgegeven station en sensor.", notFoundResult.Value); // Controleert de inhoud van de foutmelding
         }
 
@@ -69,63 +67,59 @@ namespace UCLL.Projects.WeatherStations.WebApi.Tests.Controllers
         public void StationController_GetLatestMeasurementsByStationId_ReturnOk_WhenMeasurementsFromStationExists()
         {
             // Arrange
-            var stationIds = new List<string> { "1", "2" };
+            List<string> stationIds = [ "1", "2" ];
             int measurementAmount = 1;
 
             // Voorbeelddata die de mock moet retourneren
-            var stationsWithMeasurements = new List<StationDto>
-            {
-                new StationDto
+            List<StationDto> stationsWithMeasurements =
+            [
+                new()
                 {
                     Id = "1",
                     Name = "Station A",
                     Latitude = 40.7128,
                     Longitude = -74.0060,
                     Description = "Weather station in New York",
-                    Sensors = new List<SensorDto>
-                    {
-                        new SensorDto
+                    Sensors = [
+                        new()
                         {
                             Id = 1,
                             Type = "Temperature",
-                            Measurements = new List<MeasurementDto>
-                            {
-                                new MeasurementDto { Value = 25.5, Timestamp = DateTime.Now }
-                            }
+                            Measurements = [
+                                new() { SensorValue = "25.5", Timestamp = DateTime.Now }
+                            ],
                         }
-                    }
+                    ],
                 },
-                new StationDto
+                new()
                 {
                     Id = "2",
                     Name = "Station B",
                     Latitude = 34.0522,
                     Longitude = -118.2437,
                     Description = "Weather station in Los Angeles",
-                    Sensors = new List<SensorDto>
-                    {
-                        new SensorDto
+                    Sensors = [
+                        new()
                         {
                             Id = 2,
                             Type = "Humidity",
-                            Measurements = new List<MeasurementDto>
-                            {
-                                new MeasurementDto { Value = 55.2, Timestamp = DateTime.Now }
-                            }
+                            Measurements = [
+                                new() { SensorValue = "55.2", Timestamp = DateTime.Now }
+                            ],
                         }
-                    }
+                    ],
                 }
-            };
+            ];
 
             // Stel de mock in om de voorbeelddata terug te geven
             _stationRepositoryMock.Setup(repo => repo.GetStationsLatestMeasurements(stationIds, measurementAmount)).Returns(stationsWithMeasurements);
 
             // Act
-            var result = _stationController.GetLatestMeasurementsByStationId(stationIds, measurementAmount);
+            IActionResult result = _stationController.GetLatestMeasurementsByStationId(stationIds, measurementAmount);
 
             // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result); // Controleert of het resultaat een OkObjectResult is
-            var returnValue = Assert.IsType<List<StationDto>>(okResult.Value); // Controleert of de waarde een lijst van StationDto's is
+            OkObjectResult okResult = Assert.IsType<OkObjectResult>(result); // Controleert of het resultaat een OkObjectResult is
+            List<StationDto> returnValue = Assert.IsType<List<StationDto>>(okResult.Value); // Controleert of de waarde een lijst van StationDto's is
             Assert.Equal(2, returnValue.Count); // Controleert dat er 2 stations zijn teruggegeven
 
             // Controleer de eerste stationgegevens
@@ -134,7 +128,7 @@ namespace UCLL.Projects.WeatherStations.WebApi.Tests.Controllers
             Assert.Single(returnValue[0].Sensors); // Controleert dat er precies één sensor is
             Assert.Equal("Temperature", returnValue[0].Sensors[0].Type);
             Assert.Single(returnValue[0].Sensors[0].Measurements); // Controleert dat er één meting is
-            Assert.Equal(25.5, returnValue[0].Sensors[0].Measurements[0].Value);
+            Assert.Equal("25.5", returnValue[0].Sensors[0].Measurements[0].SensorValue);
 
             // Controleer de tweede stationgegevens
             Assert.Equal("Station B", returnValue[1].Name);
@@ -142,24 +136,24 @@ namespace UCLL.Projects.WeatherStations.WebApi.Tests.Controllers
             Assert.Single(returnValue[1].Sensors); // Controleert dat er precies één sensor is
             Assert.Equal("Humidity", returnValue[1].Sensors[0].Type);
             Assert.Single(returnValue[1].Sensors[0].Measurements); // Controleert dat er één meting is
-            Assert.Equal(55.2, returnValue[1].Sensors[0].Measurements[0].Value);
+            Assert.Equal("55.2", returnValue[1].Sensors[0].Measurements[0].SensorValue);
         }
 
         [Fact]
         public void StationController_GetLatestMeasurementsByStationId_ReturnNotFound_WhenNoMeasurementsFromStationExists()
         {
             // Arrange
-            var stationIds = new List<string> { "1", "2" };
+            List<string> stationIds = [ "1", "2" ];
             int measurementAmount = 1;
 
             // Stel de mock zo in dat de repository een lege lijst van StationDto's retourneert
             _stationRepositoryMock.Setup(repo => repo.GetStationsLatestMeasurements(stationIds, measurementAmount)).Returns(new List<StationDto>());
 
             // Act
-            var result = _stationController.GetLatestMeasurementsByStationId(stationIds, measurementAmount);
+            IActionResult result = _stationController.GetLatestMeasurementsByStationId(stationIds, measurementAmount);
 
             // Assert
-            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result); // Controleert of het resultaat een NotFoundObjectResult is
+            NotFoundObjectResult notFoundResult = Assert.IsType<NotFoundObjectResult>(result); // Controleert of het resultaat een NotFoundObjectResult is
             Assert.Equal("No measurements found for the specified stations.", notFoundResult.Value); // Controleert de foutmelding
         }
     }

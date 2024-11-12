2f3be6aa8c80e3149d774c3cf8f5cf0fa31917e1
LukasLipkens <84568981+LukasLipkens@users.noreply.github.com>
2024-11-12 12:44:35 +0100

    dataservice werkt

--- src/UCLL.Projects.WeatherStations.Shared/Data/Models/Station.cs
+++ src/UCLL.Projects.WeatherStations.Shared/Data/Models/Station.cs
@@ -4,8 +4,8 @@ public class Station
 {
     public required string Id { get; set; }
     public double? BatteryLevel { get; set; } = null;
-    public double? Latitude { get; set; }
-    public double? Longitude { get; set; }
+    public double? Latitude { get; set; } = null;
+    public double? Longitude { get; set; } = null;
     public string? Name { get; set; } = null;
     public string? Description { get; set; } = null;
 

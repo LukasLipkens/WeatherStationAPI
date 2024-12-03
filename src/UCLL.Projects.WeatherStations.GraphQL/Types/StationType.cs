d10a810fb30ae7d11b35323035e19dcfdc610a41
LemmensBram <bram.lemmens2@student.ucll.be>
2024-12-03 11:43:02 +0100

    stationsensor filtering

---
+++ src/UCLL.Projects.WeatherStations.GraphQL/Types/StationType.cs
@@ -0,0 +1,14 @@
+﻿using HotChocolate.Types;
+using UCLL.Projects.WeatherStations.Shared.Data.Models;
+
+public class StationType : ObjectType<Station>
+{
+    protected override void Configure(IObjectTypeDescriptor<Station> descriptor)
+    {
+        // Configureer het stationSensors-veld
+        descriptor.Field(s => s.StationSensors)
+            .UseFiltering() // Filtering op stationSensors inschakelen
+            .UseSorting()   // Sorting op stationSensors inschakelen
+            .UseProjection(); // Voor nested data
+    }
+}
